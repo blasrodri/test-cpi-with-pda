a2afdbf5e994c58d60bf762a1e81d3baae1997d0
Dhruv D Jain <dhruv@iamsizzling.com>
2022-04-29 22:15:34 +0530

    created multiple accounts

--- programs/pupper-master/src/lib.rs
+++ programs/pupper-master/src/lib.rs
@@ -9,22 +9,25 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLns");
 pub mod pupper_master {
     use super::*;
 
-    pub fn pull_strings(ctx: Context<PullStrings>,bump: u8, data: u16) -> Result<()> {
-
+    pub fn pull_strings(ctx: Context<PullStrings>, bump: u8, data: u16) -> Result<()> {
         let cpi_program = ctx.accounts.puppet_program.to_account_info();
+
+        // msg!(&*user.to_account_info().key.to_string());
+
+        let user = &mut ctx.accounts.puppet;
+
+        let key = *user.to_account_info().key;
 
         msg!("inside puppet master");
 
-        let cpi_accounts = SetData{
+        let cpi_accounts = SetData {
             puppet: ctx.accounts.puppet.to_account_info(),
             authority: ctx.accounts.authority.to_account_info(),
         };
 
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        let bump = &[bump][..];
-        puppet_cpi::cpi::set_data(cpi_ctx.with_signer(&[&[bump][..]]), data);
-
-        
+        let seeds = &[&[b"first", key.as_ref(), bytemuck::bytes_of(&bump)][..]];
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, seeds);
+        puppet_cpi::cpi::set_data(cpi_ctx, data);
 
         Ok(())
     }
@@ -36,5 +39,5 @@ pub struct PullStrings<'info> {
     pub puppet: Account<'info, Data>,
     pub puppet_program: Program<'info, PuppetCpi>,
     /// CHECK:
-    pub authority: UncheckedAccount<'info>
+    pub authority: UncheckedAccount<'info>,
 }
